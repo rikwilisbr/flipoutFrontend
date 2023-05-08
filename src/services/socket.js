8bc38eb0fcb92e53ff3ffef20ef6556eddbd9670
Henrique William <henriquewilliamstos@gmail.com>
2023-05-08 18:53:54 -0300

    fix: socket address and testing mobile layout

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import io from 'socket.io-client'
 
-export const socket = io.connect('http://localhost:2000')
+export const socket = io.connect(process.env.REACT_APP_APIURL)
 
 
