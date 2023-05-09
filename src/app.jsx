ffc3a5f8b3aaec53bf26e8c9ef219339b2617ef5
Henrique William <henriquewilliamstos@gmail.com>
2023-05-09 12:06:51 -0300

    testing styles 5

--- src/app.jsx
+++ src/app.jsx
@@ -24,14 +24,6 @@ import {QueryClient, QueryClientProvider} from 'react-query';
 const queryClient = new QueryClient();
 
 export default function App(){
-    useEffect(()=>{
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    },[])
-    window.addEventListener('resize', () => {
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-      });
     return(
         <QueryClientProvider client={queryClient}>
             <Routes>
